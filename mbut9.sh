54fed407920f185f192c50a638de2fe1533b9b32
Bealllevey62 <bealmineey@gmail.com>
2025-10-31 19:10:39 +0700

    Update mbut9.sh

--- mbut9.sh
+++ mbut9.sh
@@ -46,7 +46,7 @@ class DetailsModificationService
         // 🚫 Batasi akses hanya untuk user ID 1
         $user = Auth::user();
         if (!$user || $user->id !== 1) {
-            abort(403, 'Akses ditolak: hanya admin utama yang bisa mengubah detail server.');
+            abort(403, 'Tolol Mau Rusuh Anjing');
         }
 
         return $this->connection->transaction(function () use ($data, $server) {
